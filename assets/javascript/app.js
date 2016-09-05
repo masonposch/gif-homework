a01470204bc2f162dc89d8600f2d1668a15c438d
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-09-05 02:05:10 -0500

    added topics buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,11 @@ $(document).ready(function(){
 var topics = ['happy', ' yes', 'no', 'shrug', 'facepalm', 'sarcastic', 'interested', 'help', 'crying', 'ugh', 'proud', 'rage', 'please', 'hello', 'goodbye'];
 
 
-
+window.onload = function(){
+	for(var i=0; i < topics.length; i++){
+		$('#theButtons').append('<button>'+topics[i]+'</button>')
+	}
+}
 
 
 
