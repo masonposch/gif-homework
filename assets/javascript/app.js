8aa0bdde7e41fd8bb08b8d15c3c600222ac02aaf
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-09-07 01:24:02 -0500

    Small changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,52 @@
 $(document).ready(function(){
 
 
-var topics = ['happy', ' yes', 'no', 'shrug', 'facepalm', 'sarcastic', 'interested', 'help', 'crying', 'ugh', 'proud', 'rage', 'please', 'hello', 'goodbye'];
+var topics = ['Happy', ' Yes', 'No', 'Shrug', 'Facepalm', 'Sarcastic', 'Interested', 'Help', 'Crying', 'Ugh', 'Proud', 'Rage', 'Please', 'Hello', 'Goodbye'];
+
+// var queryURL = "http://api.giphy.com/v1/gifs/feqkVgjJpYtjy?api_key=dc6zaTOxFJmzC";
+
+// $.ajax({url: queryURL, method: 'GET'})
+// 	 .done(function(response) {
+// 	     console.log(response);
+// 	});
 
 
 window.onload = function(){
 	for(var i=0; i < topics.length; i++){
 		$('#theButtons').append('<button>'+topics[i]+'</button>')
+		var a = $('<button>'); 
+		a.attr('data-name', topics[i]); // Added a data-attribute
 	}
 }
 
+function displayGIF(){
 
+	var emotions = $(this).attr('data-name');
+	var queryURL = "http://api.giphy.com/v1/gifs/feqkVgjJpYtjy?api_key=dc6zaTOxFJmzC";
+
+	$.ajax({url: queryURL, method: 'GET'})
+	 .done(function(response) {
+
+	 	var gifDiv = $('<div class="gif">')
+	    
+	    var rating = response.rating;
+
+
+
+
+	
+
+
+
+
+
+
+
+	});
+
+}
+
+displayGIF();
 
 
 
